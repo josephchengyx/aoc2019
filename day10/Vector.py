82674ed1a2a2070596c2e62ab64785a36d51f3b5
Joseph Cheng <josephnuva@hotmail.com>
2026-05-14 15:21:55 -0400

    Add day10 solution

---
+++ day10/Vector.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+from collections.abc import Iterator
+from dataclasses import dataclass
+from typing import Final
+from math import gcd, atan2, degrees
+
+@dataclass(unsafe_hash=True)
+class Vector2D:
+    x: Final[int]
+    y: Final[int]
+
+    def __repr__(self) -> str:
+        return f"({self.x},{self.y})"
+
+    def __add__(self, other: Vector2D) -> Vector2D:
+        return Vector2D(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other: Vector2D) -> Vector2D:
+        return Vector2D(self.x - other.x, self.y - other.y)
+
+    def __neg__(self) -> Vector2D:
+        return Vector2D(-self.x, -self.y)
+
+    def __mul__(self, scalar: int) -> Vector2D:
+        return Vector2D(self.x * scalar, self.y * scalar)
+
+    def __rmul__(self, scalar: int) -> Vector2D:
+        return self.__mul__(scalar)
+
+    def __floordiv__(self, scalar: int) -> Vector2D:
+        return Vector2D(self.x // scalar, self.y // scalar)
+
+    def dot(self, other: Vector2D) -> int:
+        return self.x * other.x + self.y * other.y
+
+    def det(self, other: Vector2D) -> int:
+        return self.x * other.y - self.y * other.x
+
+    def __iter__(self) -> Iterator[int]:
+        yield self.x
+        yield self.y
+
+    @staticmethod
+    def zero() -> Vector2D:
+        return Vector2D(0, 0)
+
+    def normalize(self) -> Vector2D:
+        norm = gcd(self.x, self.y)
+        return self // norm
+
+    def rotation_from(self, other: Vector2D) -> float:
+        return degrees(atan2(-self.det(other), self.dot(other))) % 360
+
+    def squared_euclidean_distance(self, other: Vector2D | None = None) -> int:
+        if other is None:
+            other = Vector2D.zero()
+        return (self.x - other.x) ** 2 + (self.y - other.y) ** 2
--- day10/day10.py
+++ day10/day10.py
@@ -1,29 +1,62 @@
-from Line import Coordinate2D, Line2D
+from Vector import Vector2D
+from typing import Any
 
 with open("day10_input.txt") as file:
     data = list(map(lambda line: line.rstrip(), file.readlines()))
 
-def read_asteroid_map(data: list[str]) -> set[Coordinate2D]:
-    asteroids: set[Coordinate2D] = set()
+def read_asteroid_map(data: list[str]) -> set[Vector2D]:
+    asteroids: set[Vector2D] = set()
     x_max, y_max = len(data[0]), len(data)
     for y in range(y_max):
         for x in range(x_max):
             if data[y][x] == '#':
-                asteroids.add(Coordinate2D(x, y))
+                asteroids.add(Vector2D(x, y))
     return asteroids
 
-def part1(asteroid_map: set[Coordinate2D]) -> tuple[Coordinate2D, int]:
-    visibility_scores: dict[Coordinate2D, int] = dict()
+def part1(asteroid_map: set[Vector2D]) -> tuple[Vector2D, int]:
+    visibility_scores: dict[Vector2D, int] = dict()
     for station in asteroid_map:
-        visibility_scores[station] = 0
+        lines_of_sight: set[Vector2D] = set()
         for asteroid in asteroid_map:
             if station == asteroid:
                 continue
-            line_of_sight = Line2D(station, asteroid)
-            if line_of_sight.get_points().isdisjoint(asteroid_map):
-                visibility_scores[station] += 1
+            line_of_sight = (asteroid - station).normalize()
+            lines_of_sight.add(line_of_sight)
+        visibility_scores[station] = len(lines_of_sight)
     return max(visibility_scores.items(), key=lambda item: item[1])
+
+def part2(station: Vector2D, asteroid_map: set[Vector2D]) -> int:
+    def unravel_colwise(lst: list[list[Any]]) -> list[Any]:
+        unravelled: list[Any] = list()
+        rows, cols = len(lst), max(map(lambda nested: len(nested), lst))
+        for c in range(cols):
+            for r in range(rows):
+                if c >= len(lst[r]):
+                    continue
+                unravelled.append(lst[r][c])
+        return unravelled
+
+    up_axis = Vector2D(0, -1)
+    laser_orientations: dict[Vector2D, list[Vector2D]] = dict()
+    for asteroid in asteroid_map:
+        if station == asteroid:
+            continue
+        orientation = (asteroid - station).normalize()
+        if orientation not in laser_orientations:
+            laser_orientations[orientation] = list()
+        laser_orientations[orientation].append(asteroid)
+    for orientation, asteroids in laser_orientations.items():
+        asteroids.sort(key=lambda asteroid: asteroid.squared_euclidean_distance(station))
+    laser_orientations: list[tuple[Vector2D, list[Vector2D]]] \
+        = list(laser_orientations.items())
+    laser_orientations.sort(key=lambda orientation: orientation[0].rotation_from(up_axis))
+    laser_orientations: list[list[Vector2D]] \
+        = list(map(lambda orientation: orientation[1], laser_orientations))
+    laser_orientations: list[Vector2D] = unravel_colwise(laser_orientations)
+    asteroid_200 = laser_orientations[200-1]
+    return asteroid_200.x * 100 + asteroid_200.y
 
 asteroid_map = read_asteroid_map(data)
 station, visibility_score = part1(asteroid_map)
 print(f"Part 1: {visibility_score}")
+print(f"Part 2: {part2(station, asteroid_map)}")
